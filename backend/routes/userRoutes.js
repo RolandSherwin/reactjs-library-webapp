70037cc6a36596167f3b9ad5fd34a2ea6b066451
RolandSherwin <RolandSherwin@protonmail.com>
2022-02-20 22:41:45 +0530

    user api to get book data

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,4 +11,37 @@ router.get("/getAllUsers", async (req, res, next) => {
     }
 });
 
+router.get("/getUserReservedBooks/:userId", async (req, res, next) => {
+    try{
+        let reservedBooks = await userService.getUserReservedBooks(req.params.userId);
+        res.json(reservedBooks);
+    } catch(err){
+        next(err);
+    }
+})
+router.get("/getUserReservedBooks/:userId", async (req, res, next) => {
+    try{
+        let reservedBooks = await userService.getUserReservedBooks(req.params.userId);
+        res.json(reservedBooks);
+    } catch(err){
+        next(err);
+    }
+})
+router.get("/getUserBorrowedBooks/:userId", async (req, res, next) => {
+    try{
+        let borrowedBooks = await userService.getUserBorrowedBooks(req.params.userId);
+        res.json(borrowedBooks);
+    } catch(err){
+        next(err);
+    }
+})
+router.get("/getUserBookDetails/:userId", async (req, res, next) => {
+    try{
+        let bookDetails = await userService.getUserBookDetails(req.params.userId);
+        res.json(bookDetails);
+    } catch(err){
+        next(err);
+    }
+})
+
 module.exports = router;
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -12,7 +12,35 @@ exports.getAllUsers = async () => {
     return userList;
 };
 
-exports.getUserBookingData = async () => {
+exports.getUserBorrowedBooks = async (userId) => {
     const userCollection = await connection.getUserCollection();
-    let;
+    let borrowed = await userCollection.find({'userId':userId}, {_id:0, borrowedBooks:1});
+    if (!borrowed){
+        let err = new Error("getUserBorrowedBooks(): Error finding by userId");
+        err.status = 500;
+        throw err;
+    }
+    return borrowed;
 };
+
+exports.getUserReservedBooks = async (userId) => {
+    const userCollection = await connection.getUserCollection();
+    let reserved = await userCollection.find({'userId':userId}, {_id:0, reservedBooks:1});
+    if (!reserved){
+        let err = new Error("getUserReservedBooks(): Error finding by userId");
+        err.status = 500;
+        throw err;
+    }
+    return reserved;
+};
+
+exports.getUserBookDetails = async (userId) => {
+    const userCollection = await connection.getUserCollection();
+    let bookDetails = await userCollection.find({'userId':userId}, {_id:0, reservedBooks:1, borrowedBooks:1});
+    if (!bookDetails){
+        let err = new Error("getUserBookDetails(): Error finding by userId");
+        err.status = 500;
+        throw err;
+    }
+    return bookDetails;
+}
